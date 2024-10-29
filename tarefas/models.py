70b91c9f4a76fac03e51a9aabb2702091755af1a
Artur Henrique de Lima Teixeira <221.14.035@EDU.gruposete.local>
2024-10-28 22:34:24 -0300

    Descrição breve das mudanças

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_criacao = models.DateTimeField(auto_created=True)
+    conluida = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titulo
 # Create your models here.
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Tarefa
+
 
 def lista_tarefas(request):
-    tarefas = [
-        "Estudar Django",
-        "Criar um projeto",
-        "Revisar o conteúdo"
-    ]
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        if titulo:
+            Tarefa.objects.create(titulo=titulo)
+            return redirect('lista_tarefas')
+    tarefas = Tarefa.objects.all()
     return render(request, 'lista_tarefas.html', {'tarefas': tarefas})
-
-# Create your views here.
